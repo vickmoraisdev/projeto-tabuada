bc34cbf203f0b1a7465675d63e6576269b749e55
Vitória Morais <vitoriamoraisfaria@gmail.com>
2026-05-21 14:10:59 -0300

    Implementei JS. Primeira versão do JS

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 function result(){
-    
+    let txtnum = document.getElementById('txtnum')
+    let num = Number(txtnum.value)
+    let res = document.getElementById('res')
+
+    res.innerHTML = ''
+    if(txtnum.value === ''){
+        window.alert('[ERRO] Informe um valor válido.')
+        res.innerHTML = 'Digite um número acima'
+    } else{
+        for(let c = 1; c <= 10; c++){
+            res.innerHTML += `${num} x ${c} = ${num*c} <br>`
+        }
+    }
 }
